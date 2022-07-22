38ef7bfef75b7ec7a6f3022acb636ef7602c6db6
CrystalDev1 <99578334+CrystalDev1@users.noreply.github.com>
2022-07-22 00:09:50 -0500

    Add files via upload

---
+++ auto_fish.lua
@@ -0,0 +1,31 @@
+local toggled = false
+local uis = game:GetService('UserInputService')
+
+uis.InputBegan:Connect(function(key, processed) 
+    if processed then
+        return
+    end
+
+    if key.KeyCode == Enum.KeyCode.L then 
+        toggled = not toggled
+    end
+    
+end)
+
+local g = _G
+g['scriptValue'] = (g['scriptValue'] and g["scriptValue"] + 1) or 1
+local ScriptValue = g['scriptValue']
+-- aaa
+local rs = game:GetService("RunService")
+while true do 
+    rs.Heartbeat:Wait()
+    if ScriptValue == g['scriptValue'] then
+        if toggled then 
+            print'fish?'
+            game:GetService("ReplicatedStorage").Modules.GlobalInit.RemoteEvents.PlayerCatchFish:FireServer()
+            wait(12)
+        end 
+    else 
+        break
+    end
+end 
